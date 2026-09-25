3e06cc0ae43b20516e4ace7ff9ac83069b07975d
rlaaaa <orangemodel7@gmail.com>
2026-09-25 18:15:19 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -8,10 +8,22 @@ const urlsToCache = [
 ];
 
 self.addEventListener('install', event => {
+  self.skipWaiting(); // 새로운 버전이 발견되면 즉시 설치 및 대기 상태 건너뛰기
   event.waitUntil(
     caches.open(CACHE_NAME)
       .then(cache => cache.addAll(urlsToCache))
   );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.filter(name => name !== CACHE_NAME).map(name => caches.delete(name))
+      );
+    })
+  );
+  self.clients.claim(); // 즉시 페이지 제어권 가져오기
 });
 
 self.addEventListener('fetch', event => {
